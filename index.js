7cb49c37c358953dfb5d96952c3a95e8771bd5f1
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-29 12:58:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const options = [
+  cors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+];
 
+app.use(options);
 
 app.get('/updates', (req, res) => {
     res.json(JSON.parse(require('fs').readFileSync((path.join(process.cwd(), 'assets') + '/updates.json'), 'utf-8')));

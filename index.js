5ca9b739fb82fdf7305742cfe15e5efc3a09ae5e
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-29 12:45:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.get('/updates', (req, res) => {
     res.json(JSON.parse(require('fs').readFileSync((path.join(process.cwd(), 'assets') + '/updates.json'), 'utf-8')));
 });
-
 
 app.listen(4000);
 
